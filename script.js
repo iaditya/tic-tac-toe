fcbbdd8909866c2b3b769667295f81b6ee233660
Aditya Gupta <iadityapansari@gmail.com>
2017-09-16 02:53:35 +0530

    implements turn function

--- script.js
+++ script.js
@@ -16,3 +16,28 @@ const winningComb = [
 
 const cells = document.querySelectorAll('.cell');
 console.log(typeof cells);
+
+startGame();
+
+function startGame() {
+    document.querySelector('.endgame').style.display = "none";
+    origBoard = Array.from(Array(9).keys());
+    //console.log(origBoard);
+
+    for(var i=0; i< cells.length; i++) {
+        cells[i].innerText = '';
+        cells[i].style.removeProperty = 'background-color';
+        cells[i].addEventListener('click', turnClick, false);
+    }
+
+    function turnClick(squre) {
+        turn(squre.target.id, humPlayer);
+        //console.log(e.target.id);
+    }
+
+    function turn (squreId, player) {
+        console.log(player);
+        origBoard[squreId] = player;
+        document.getElementById(squreId).innerText = player;
+    }
+}
